83ac8a2e8ad73f29fe98071eb0dbc76c5d115b42
KasiaGP <kasiagrochal@hotmail.com>
2021-11-10 12:40:05 +0100

    initial commit

---
+++ src/main/java/Employee.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Employee {
+    Scanner input = new Scanner(System.in);
+
+    private String firstName;
+    private String lastName;
+    private double salary;
+
+    public void addDetails(){
+        System.out.println("You are adding a new employee.");
+        System.out.println("First name:");
+        setFirstName(input.nextLine());
+        System.out.println("Last name:");
+        setLastName(input.nextLine());
+        System.out.println("Salary: ");
+        setSalary(input.nextDouble());
+        input.nextLine();
+        System.out.println("You have successfully added an employee\n");
+    }
+
+    public void getAllData() {
+        System.out.println("Salary for " + getFirstName() + " " + getLastName() + " is " + getSalary());
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean quit = false;
+        List<Employee> employeeList = new ArrayList<>();
+
+        for (int i = 0; i <5 ; i++) {
+            employeeList.add(addNewEmployee());
+        }
+        while (!quit) {
+            printMainMenu();
+            int action = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (action) {
+                case 1:
+                    getSalaryTotal(employeeList);
+                    break;
+                case 2:
+                    getAllData(employeeList);
+                    break;
+                case 3:
+                    employeeList.add(addNewEmployee());
+                    break;
+                case 4:
+                    quit = true;
+                    System.out.println("Closing the app...");
+                    break;
+            }
+        }
+    }
+
+    public static void printMainMenu() {
+        System.out.println("\nPlease type an integer to choose the corresponding action:\n" +
+                "1 - Print sum of all employees salary\n" +
+                "2 - Display all employees data\n" +
+                "3 - Add new employee\n" +
+                "4 - End program\n");
+    }
+
+    public static Employee addNewEmployee() {
+        Employee newEmployee = new Employee();
+        newEmployee.addDetails();
+        return newEmployee;
+    }
+
+    public static void getSalaryTotal(List<Employee> list) {
+        double salary = 0;
+        for (Employee employee : list) {
+            salary += employee.getSalary();
+        }
+        System.out.println("Sum of the salaries is " + salary + "\n");
+    }
+
+    public static void getAllData(List<Employee> list) {
+        for (Employee employee : list) {
+            employee.getAllData();
+        }
+    }
+}
